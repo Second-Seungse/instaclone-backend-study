3b52f3132fc59f810d84ea9268db88ca2f33f594
Seungse Oh <ohsse12521@gmail.com>
2021-05-24 21:20:26 +0900

    1.6 Prisma Client

--- server.js
+++ server.js
@@ -1,44 +1,53 @@
+import { PrismaClient } from ".prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient()
 
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id: Int!): Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: String!): Boolean
   }
 `;
 
 const resolvers = {
-    Query: {
-        movies: () => [],
-        movie: () => ({ title: "Hello", year: 2021 }),
-    },
-    Mutation: {
-        createMovie: (_, { title }) => {
-            console.log(title);
-            return true;
+  Query: {
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
         },
-        deleteMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        },
+      }),
+    deleteMovie: (_, { id }) => {
+      return true;
     },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 
 server
-    .listen()
-    .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
