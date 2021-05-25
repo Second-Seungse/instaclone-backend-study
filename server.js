80330a3017a48d1d64e4d9423d021b95a78769eb
Seungse Oh <ohsse12521@gmail.com>
2021-05-25 13:22:04 +0900

    #4.1 Create Account part Two

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ const server = new ApolloServer({
 const PORT = process.env.PORT;
 server
   .listen(PORT)
-  .then(() => console.log(`🚀😆 Server is running on http://localhost:${PORT}/`));
+  .then(() =>
+    console.log(`🚀😆 Server is running on http://localhost:${PORT}/`)
+  );
