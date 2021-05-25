a6c4a7771c5b827f3eb50b952aaa59eaa4f5e70f
Seungse Oh <ohsse12521@gmail.com>
2021-05-25 14:09:08 +0900

    #4.3 seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,32 +7,39 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      // * Check if username or email are already on DB.
-      // findFirst : 조건에 맞는 첫 번째 결과를 return
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
-      // * hash password
-      // * save and return the user
+      try {
+        // * Check if username or email are already on DB.
+        // findFirst : 조건에 맞는 첫 번째 결과를 return
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/password is already taken😅");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+        // * hash password
+        // * save and return the user
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
