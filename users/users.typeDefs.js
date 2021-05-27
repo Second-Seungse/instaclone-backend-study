633ebcf0b163a2ef73df5a0eb1cc4c6f5e3efdd5
Seungse Oh <ohsse12521@gmail.com>
2021-05-27 23:25:32 +0900

    #4.26 Computed Fields part One

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -11,7 +11,12 @@ export default gql`
     avatar: String
     following: [User]
     followers: [User]
+    totalFollowing: Int!
+    totalFollowers: Int!
     createdAt: String!
     updatedAt: String!
   }
 `;
+
+//isFollowing: Boolean!
+//isMe: Boolean!
