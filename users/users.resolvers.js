82d16936139110e8b4643000cf64fad7dcb5c34b
Seungse Oh <ohsse12521@gmail.com>
2021-05-27 23:49:45 +0900

    #4.27 Computed Fields part Two

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -1,9 +1,34 @@
+import client from "../client";
+
 export default {
-    User: {
-      totalFollowing: (root) => {
-        console.log(root.username);
-        return 0;
-      },
-      totalFollowers: () => 999,
+  User: {
+    // * 내 id를 팔로워로 보유하고있는 id를 count한다.
+    totalFollowing: ({ id }) =>
+      client.user.count({
+        where: {
+          followers: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+    // * 내 id를 팔로윙하고 있는 id를 count한다.
+    totalFollowers: ({ id }) =>
+      client.user.count({
+        where: {
+          following: {
+            some: {
+              id,
+            },
+          },
+        },
+      }),
+    isMe: ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      return id === loggedInUser.id;
     },
-  };
+  },
+};
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -1,5 +1,6 @@
 import { gql } from "apollo-server";
 
+// * totalFollowing, totalFollowers 등 DB에 없는 값을 조회하는경우 resolver를 찾아 참조한다.
 export default gql`
   type User {
     id: String!
@@ -13,10 +14,10 @@ export default gql`
     followers: [User]
     totalFollowing: Int!
     totalFollowers: Int!
+    isMe: Boolean!
     createdAt: String!
     updatedAt: String!
   }
 `;
 
 //isFollowing: Boolean!
-//isMe: Boolean!
