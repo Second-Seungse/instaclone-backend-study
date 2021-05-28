443d994fe039802510a01ad48f2cc61474697551
Seungse Oh <ohsse12521@gmail.com>
2021-05-28 16:38:37 +0900

    #6.11 seeLikes

--- photos/seePhotoLikes/seePhotoLikes.resolvers.js
+++ photos/seePhotoLikes/seePhotoLikes.resolvers.js
@@ -11,6 +11,7 @@ export default {
           user: true,
         },
       });
+      // * like마다 like.user를 찾아서 user만 가진 새로운 array를 return한다.
       return likes.map((like) => like.user);
     },
   },
