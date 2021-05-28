6276789dd013acb9725a656a7b28396a6a081a31
Seungse Oh <ohsse12521@gmail.com>
2021-05-28 20:25:41 +0900

    #6.19 S3 Photo Upload

--- users/users.utils.js
+++ users/users.utils.js
@@ -25,11 +25,19 @@ export const protectResolver = (user) => {
 
 export const protectedResolver =
   (ourResolver) => (root, args, context, info) => {
+    console.log(info.operation);
+    // * 로그인이 되지 않은 상태인 경우
     if (!context.loggedInUser) {
-      return {
-        ok: false,
-        error: "Please login to perform this action.",
-      };
+      // * 로그인이 필요한 query는 전부 ok와 error를 정의해야하는 번거로움을 해결
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "Please log in to perform this action.",
+        };
+      }
     }
     return ourResolver(root, args, context, info);
   };
