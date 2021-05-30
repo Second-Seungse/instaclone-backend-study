fc4f951d94c00a2c182110966e9699dd96ac3400
Seungse Oh <ohsse12521@gmail.com>
2021-05-30 11:31:57 +0900

    #7.10 Filtering Subscriptions part Two

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -6,12 +6,27 @@ import pubsub from "../../pubsub";
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW_MESSAGE),
-        ({ roomUpdates }, { id }) => {
-          return roomUpdates.roomId === id;
+      subscribe: async (root, args, context, info) => {
+        // * 요청하는 ID의 Room이 존재하는지 검증
+        const room = await client.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!room) {
+          throw new Error("You shall not see this.");
         }
-      ),
-    },
+        // ? 이 부분이 확실하게 이해가 가지는 않는다.
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(root, args, context, info);
+      },  // subscribe
+    },  // roomUpdates
   },
 };
