1156a4300795973c716983085a3d33360b36d3f7
Seungse Oh <ohsse12521@gmail.com>
2021-05-30 10:30:09 +0900

    #7.7 Subscriptions Setup part One

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
+import pubsub from "./pubsub";
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
@@ -18,6 +19,7 @@ const apollo = new ApolloServer({
 });
 
 const app = express();
+apollo.installSubscriptionHandlers(app);
 app.use(logger("tiny"));
 app.use("/static", express.static("uploads"));
 apollo.applyMiddleware({ app });
