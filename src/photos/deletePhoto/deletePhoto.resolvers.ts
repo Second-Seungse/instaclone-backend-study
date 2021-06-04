da47a2a163a6d245507363145dc0404d55c8cc82
SeungseOh <ohsse12521@gmail.com>
2021-06-04 22:16:42 +0900

    #11.11 Comments part One

--- src/photos/deletePhoto/deletePhoto.resolvers.ts
+++ src/photos/deletePhoto/deletePhoto.resolvers.ts
@@ -2,6 +2,8 @@ import { protectedResolver } from "../../users/users.utils";
 import { deleteFromS3 } from "../../shared/shared.utils";
 import { Resolvers } from "../../types";
 
+// TODO comment가 달린 photo의 경우 삭제가 안된다. photo를 삭제할 경우 comment까지 삭제 필요
+
 const resolver: Resolvers = {
   Mutation: {
     deletePhoto: protectedResolver(
