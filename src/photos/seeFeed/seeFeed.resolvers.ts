b443accc417ba34ce488effd208d167f00e99781
SeungseOh <ohsse12521@gmail.com>
2021-06-12 14:50:42 +0900

    #16.6 Search part Four

--- src/photos/seeFeed/seeFeed.resolvers.ts
+++ src/photos/seeFeed/seeFeed.resolvers.ts
@@ -3,9 +3,11 @@ import { protectedResolver } from "../../users/users.utils";
 
 const resolver: Resolvers = {
   Query: {
-    seeFeed: protectedResolver((_, __, { client, loggedInUser }) =>
+    seeFeed: protectedResolver((_, { offset }, { client, loggedInUser }) =>
       client.photo.findMany({
         // * 팔로워 목록에 내 아이디가 있는 유저들의 photo를 찾는다.
+        take: 2,
+        skip: offset,
         where: {
           OR: [
             {
